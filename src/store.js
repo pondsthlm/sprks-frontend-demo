c50775c0b1e15c890f251a10ef5d4d87b2f4d224
Max Berkenstam <max.berkenstam@gmail.com>
2019-10-02 10:23:38 +0200

    Log state changes on dev

--- src/store.js
+++ src/store.js
@@ -36,9 +36,19 @@ function reducer(state, action) {
   }
 }
 
+const logger = (action, prevState, currentState) => {
+  // https://medium.com/strands-tech-corner/react-state-management-without-redux-d39c7087039d
+  if (process.env.NODE_ENV !== 'development') return;
+  console.groupCollapsed('Logger');
+  console.log('%c Action:', 'color: blue', action);
+  console.log('%c Previous State:', 'color: red', prevState);
+  console.log('%c Current State:', 'color: green', currentState);
+  console.groupEnd();
+};
+
 const reducerMiddleWare = (state, action) => {
   const newState = reducer(state, action);
-  console.log(newState);
+  logger(action, state, newState);
   return newState;
 };
 
